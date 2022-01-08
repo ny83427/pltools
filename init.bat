c4a33a0b9fab2ca38cb29b1700839b1fcc7ddc9d
Nathanael Yang <41705526+ny83427@users.noreply.github.com>
2022-01-08 08:45:39 -0600

    Update init.bat

--- init.bat
+++ init.bat
@@ -1,3 +1,7 @@
 @echo off
+:: https://chocolatey.org/install
+:: Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+:: cinst python -y
+:: 使用Chocolatey安装PYTHON运行环境先
 mkdir C:\Tmp\pltools
 pip install selenium chromedriver-autoinstaller
